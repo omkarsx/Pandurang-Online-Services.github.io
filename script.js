28b119346a6cb7153e7679b88b15996a780190e5
Omkar Shinde <obs212119@gmail.com>
2026-05-13 11:05:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -192,3 +192,27 @@ document.querySelectorAll('a[href^="#"]').forEach(a => {
     }
   });
 });
+
+// THEME TOGGLE
+
+const themeToggle =
+document.getElementById('themeToggle');
+
+const savedTheme =
+localStorage.getItem('theme');
+
+if(savedTheme === 'dark'){
+document.body.classList.add('dark-mode');
+}
+
+themeToggle.addEventListener('click', () => {
+
+document.body.classList.toggle('dark-mode');
+
+if(document.body.classList.contains('dark-mode')){
+localStorage.setItem('theme','dark');
+}else{
+localStorage.setItem('theme','light');
+}
+
+});
